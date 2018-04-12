a8a699c038555937ffbce28d6fdfe25bec93b75b
Ryan Miller <rm.miller83@gmail.com>
2018-04-12 14:06:39 -0400

    Initial Commit

---
+++ TalkBoy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TalkBoy
+//
+//  Created by Ryan Miller on 4/12/18.
+//  Copyright © 2018 Ryan Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
